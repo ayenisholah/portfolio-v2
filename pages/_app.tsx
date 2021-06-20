afe3c887da5c511a943a8f9af7498cda83ff6a32
Shola Ayeni <ayenis@yahoo.com>
2021-06-20 10:14:27 +0100

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url:string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
-export default MyApp
+
+export default App
