7aad8ece6c1dd778b528a41023f723ef8f380718
Shola Ayeni <ayenis@yahoo.com>
2021-07-01 09:02:56 +0100

    lint config and fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,23 @@
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
-import '../styles/globals.css'
+import React from "react";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    const handleRouteChange = (url:string) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-  return <Component {...pageProps} />
-}
+  return <Component {...pageProps} />;
+};
 
-export default App
+export default App;
